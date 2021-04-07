9cbcfe3c88ccb7c4c04881525186eabc70e35669
MattiaSoppelsa <81378995+MattiaSoppelsa@users.noreply.github.com>
2021-04-07 12:36:42 +0200

    Update R_code_time_series_analysis.r

--- R_code_time_series_analysis.r
+++ R_code_time_series_analysis.r
@@ -9,3 +9,13 @@ setwd("D:/lab/greenland") #setting the folder we will use
 #import single file/dataset with the function raster()
 lst_2000<-raster("lst_2000.tif") 
 plot(lst_2000) #we can check if the import is done.
+#import also the other files
+lst_2005<-raster("lst_2005.tif") 
+lst_2010<-raster("lst_2010.tif") 
+lst_2015<-raster("lst_2015.tif") 
+#exercise with par
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
