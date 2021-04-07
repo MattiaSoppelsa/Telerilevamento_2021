d0a5e19444e2d674edd75cf497101ddfbe5ed044
MattiaSoppelsa <81378995+MattiaSoppelsa@users.noreply.github.com>
2021-04-07 13:04:07 +0200

    Update R_code_time_series_analysis.r

--- R_code_time_series_analysis.r
+++ R_code_time_series_analysis.r
@@ -19,3 +19,18 @@ plot(lst_2000)
 plot(lst_2005)
 plot(lst_2010)
 plot(lst_2015)
+
+#how to import all the file in the same time
+#first at all we have to create a list with all files with using a pattern, and named 
+rlist<-list.files(pattern="lst")
+rlist
+
+#using import<- lapply(rlist,raster)
+#lapply serve per applicare la funzione raster a tutta la lista appena creata, o meglio "selezionata"
+import<- lapply(rlist,raster)
+import
+
+TGr<-stack(import) 
+
+plotRGB(TGr, 1, 2, 3, stretch="Lin") #2000/2005/2010 all together.
+plotRGB(TGr, 2, 3, 4, stretch="Lin") #plot rgb con valori satellitari che riguardano la temperatura
