977514a99e72012428afd802a76d7923edd51973
MattiaSoppelsa <81378995+MattiaSoppelsa@users.noreply.github.com>
2021-04-16 11:28:40 +0200

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,11 @@
+#R_code_knitr.r
+
+setwd("D:/lab/")
+
+
+
+#per esportazione file in pdf (file r -> file TEX -> pdf
+library(knitr)
+
+# stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+stitch("D:/lab/greenland/R_code_greenland.r.txt/", template=system.file("misc", "knitr-template.Rnw", package="knitr")) #serve .txt finale 
