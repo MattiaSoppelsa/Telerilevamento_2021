12496a9c34e40ca761b681eca2e25422ff741c95
MattiaSoppelsa <81378995+MattiaSoppelsa@users.noreply.github.com>
2021-04-16 12:00:45 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -7,5 +7,8 @@ setwd("D:/lab/")
 #per esportazione file in pdf (file r -> file TEX -> pdf
 library(knitr)
 
-# stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
-stitch("D:/lab/greenland/R_code_greenland.r.txt/", template=system.file("misc", "knitr-template.Rnw", package="knitr")) #serve .txt finale 
+#tinytex::install_tinytex()
+
+# stitch("R_code_greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+stitch("D:/lab/greenland/R_code_greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr")) #serve .txt finale 
+
