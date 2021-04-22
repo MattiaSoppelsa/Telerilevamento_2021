b673fa4c3bb9f7494911b59c8b6cbdfc628b28bd
MattiaSoppelsa <81378995+MattiaSoppelsa@users.noreply.github.com>
2021-04-22 10:24:47 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -2,7 +2,7 @@
 
 #if already installed cal back the packages
 library(raster)
-library(Rtoolbox)
+library(RStoolbox)
 
 #set the folder with we will work
 setwd("D:/lab/solar")
@@ -13,3 +13,13 @@ so # to check the levels we have
 
 #visualize RGB levels
 plotRGB(so, 1, 2, 3, stretch="lin")
+
+#Unsupervised Classification 
+soc<-unsuperClass(so, nClasses=3)
+#per plottare usiamo l'immagine classificata "soc" e la leghiamo al modello della mappa
+plot(soc$map)
+
+#Unsupervised Classification with 20 classes
+soc<-unsuperClass(so, nClasses=20)
+
+plot(soc$map)
