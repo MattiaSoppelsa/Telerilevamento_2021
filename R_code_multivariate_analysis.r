b5ed4560d31411e71621a6d878024a9624a8b40f
MattiaSoppelsa <81378995+MattiaSoppelsa@users.noreply.github.com>
2021-04-28 12:45:09 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -21,3 +21,31 @@ plot(p224r63_2011$B2_sre, p224r63_2011$B1_sre, col="red", pch=19, cex=2)
 #per farlo per tutte le bande? usiamo la funzione "pairs"
 #per plottare tutte le correlazioni possibili (2 a 2) tra le variabili (ovvero tutte le bande)
 pairs(p224r63_2011)
+
+
+#aggregate function
+#per diminuire o allargare la larghezza dei pixel, quindi se vogliamo modificare la risoluzione
+#aggregate cells, res: resampling (ricampionamento)
+p224r63_2011res <- aggregate(p224r63_2011, fact=10)
+
+p224r63_2011res #vedo che ho aumentato la grandezza del pixel quindi ho ridotto la risoluzione
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2,stretch="lin")
+plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="lin")
+
+#PCA principal component analysis per ridurre n dimensioni in un numero minore. 
+p224r63_2011res_pca <- rasterPCA(p224r63_2011res)
+# ma viene generato un file con all'interno un modello, una mappa etc..
+
+summary(p224r63_2011res_pca$model) #per vedere la varianza delle componenti 
+# e vedo che la componente 1 (PC1) spiega il 99% della variabilità. Possiede la maggior parte dell'informazione    
+# l'ultime componenti avranno un'informazione minima, sarà rumore.                                 
+                                 
+
+p224r63_2011res_pca 
+
+plotRGB(p224r63_2011res_pca$map, r=1, g=2, b=3, stretch="lin") #map 
+                                 
+str(p224r63_2011res_pca)  #per più informazioni, anche sulla struttura       
+
